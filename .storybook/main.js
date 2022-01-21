8f9abc2ac35ceb0dd43feb481b8b90a36e0eae3d
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2022-01-21 13:28:13 -0300

    Storybook adaptations to v6 (Essentials and Controls)

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,9 +4,7 @@ module.exports = {
     "../src/components/**/stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
-    "@storybook/addon-actions",
-    "@storybook/addon-links",
-    "@storybook/addon-knobs/register"
+    "@storybook/addon-essentials"
   ],
   "framework": "@storybook/react"
 }
